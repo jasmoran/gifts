3ecfb96f2e7ed3562586c3fffcdc74c59d6b51ec
Jack Moran <jack.moran@magiqsoftware.com>
2021-01-09 14:55:50 +1300

    V3

--- gifts.js
+++ gifts.js
@@ -45,38 +45,85 @@ auth.onAuthStateChanged(function(user) {
   }
 });
 
+function sortBy (uid, claimedBy) {
+  if (claimedBy === uid) return 0
+  if (!claimedBy) return 1
+  return 2
+}
+
+function renderGift (gift) {
+  if (!gift.title || gift.ignore) return '';
+  
+  var innerHTML = ''
+  innerHTML += '<div class="column col-6 col-xs-12"><div class="card"><div class="card-image"><img class="img-responsive p-centered" style="max-height: 250px;" src="'
+  innerHTML += gift.image || './gift.png'
+  innerHTML += '"></div><div class="card-header"><div class="card-title h5">'
+  innerHTML += gift.title
+  innerHTML += '</div></div><div class="card-body">'
+  innerHTML += gift.description || ''
+  if (gift.links) {
+    innerHTML += '<div class="navbar-section"><br /><div class="h6">Ideas</div>'
+    gift.links.forEach(function (link) {
+      innerHTML += '<a class="btn btn-link" href="' + link.url + '">' + link.text + '</a>'
+    })
+    innerHTML += '</div>'
+  }
+  innerHTML += '</div>'
+
+  if (gift.claimable) {
+    innerHTML += '<div class="card-footer">'
+    switch (gift.claim) {
+      case 'self':
+        innerHTML += '<b>You are getting this</b><div class="btn btn-primary float-right" onClick="unclaim(\''
+        innerHTML += gift.id
+        innerHTML += '\')">Changed your mind?</div>'
+        break;
+      case 'none':
+        innerHTML += '<div class="btn btn-primary" onClick="claim(\''
+        innerHTML += gift.id
+        innerHTML += '\')">I’ll get this</div>'
+        break;
+      case 'other':
+        innerHTML += '<b>Someone else is getting this</b>'
+        break;
+    }
+    innerHTML += '</div>'
+  }
+
+  innerHTML += '</div></div>'
+
+  return innerHTML
+}
+
 function render () {
   if (!gifts || !claims) return
 
   var uid = auth.currentUser ? auth.currentUser.uid : null;
 
-  var innerHTML = ''
+  var selfClaimed = []
+  var unclaimed = []
+  var otherClaimed = []
+  gifts.sort(function (b, a) { return b.title.localeCompare(a.title) })
   gifts.forEach(function (gift) {
     var claimedBy = claims[gift.id]
-
-    if (!gift.title) return;
-    if (claimedBy && claimedBy !== uid && gift.claimable) return;
-
-    innerHTML += '<div class="column col-6 col-xs-12"><div class="card"><div class="card-image"><img class="img-responsive p-centered" style="max-height: 250px;" src="'
-    innerHTML += gift.image || './gift.png'
-    innerHTML += '"></div><div class="card-header"><div class="card-title h5">'
-    innerHTML += gift.title
-    innerHTML += '</div></div><div class="card-body">'
-    innerHTML += gift.description || ''
-    innerHTML += '</div>'
-    if (gift.claimable) {
-      if (claimedBy) {
-        innerHTML += '<div class="card-footer"><b>You are getting this</b><div class="btn btn-primary float-right" onClick="unclaim(\''
-        innerHTML += gift.id
-        innerHTML += '\')">Changed your mind?</div></div>'
-      } else {
-        innerHTML += '<div class="card-footer"><div class="btn btn-primary" onClick="claim(\''
-        innerHTML += gift.id
-        innerHTML += '\')">I’ll get this</div></div>'
-      }
+    if (claimedBy === uid) {
+      gift.claim = 'self'
+      selfClaimed.push(gift)
+    } else if (!claimedBy) {
+      gift.claim = 'none'
+      unclaimed.push(gift)
+    } else {
+      gift.claim = 'other'
+      otherClaimed.push(gift)
     }
-    innerHTML += '</div></div>'
   })
+
+  var innerHTML = ''
+  selfClaimed.forEach(function (gift) { innerHTML += renderGift(gift) })
+  innerHTML += '<div class="column col-6 col-xs-12"><div class="card"><div class="card-image"><img class="img-responsive p-centered" style="max-height: 250px;" src="https://www.aucklandcitymission.org.nz/wp-content/uploads/2015/12/logo.png"></div><div class="card-header"><div class="card-title h5">Donate to Auckland City Mission</div></div><div class="card-body">Donate an amount in our name to Auckland City Mission</div><div class="card-footer"><a class="btn btn-primary" href="https://www.aucklandcitymission.org.nz/support-us">Donate</a></div></div></div>'
+  innerHTML += '<div class="column col-6 col-xs-12"><div class="card"><div class="card-image"><img class="img-responsive p-centered" style="max-height: 250px;" src="https://www.aunties.co.nz/wp-content/uploads/2020/01/new-aunties-logo.jpg"></div><div class="card-header"><div class="card-title h5">Donate to The Aunties</div></div><div class="card-body">Donate an amount in our name to The Aunties</div><div class="card-footer"><a class="btn btn-primary" href="https://www.aunties.co.nz/donate/">Donate</a></div></div></div>'
+  unclaimed.forEach(function (gift) { innerHTML += renderGift(gift) })
+  otherClaimed.forEach(function (gift) { innerHTML += renderGift(gift) })
 
   if (cards.innerHTML !== innerHTML) {
     cards.innerHTML = innerHTML
